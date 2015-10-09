a32ed3ac9d0cf7051fa70e518c8c4b308bc8f263
Klint Tiley <kctiley@gmail.com>
2015-10-09 09:25:35 -0600

    Test passing

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
+
 module.exports = {
 
-  //Write a function that takes nested arrays and turns it into nested objects. If you solve it one way, then solve it another.
-//-> {
-//   "1934356360": {
-//     "title": "Learn to Program",
-//     "author": "Chris Pine",
-//     "isbn10": 1934356360
-//   },
-//   "1593275846": {
-//     "title": "Eloquent Javascript",
-//     "author": "Marijn Haverbeke",
-//     "isbn10": 1593275846
+  bookData: function(inputArr){
+      var resultObj = {};
+      var subObj = {};
+      for (var i = 0; i < inputArr.length; i++){
+          resultObj[inputArr[i][2]] = {};
+          resultObj[inputArr[i][2]]['title'] = inputArr[i][0];
+          resultObj[inputArr[i][2]]['author'] = inputArr[i][1];
+          resultObj[inputArr[i][2]]['isbn10'] = inputArr[i][2];
+      }
+      return resultObj
+  }
+
+}
+
+// OR
+
+// module.exports = function(inputArr){
+//       var resultObj = {};
+//       for (var i = 0; i < inputArr.length; i++){
+//           resultObj[inputArr[i][2]] = {};
+//           resultObj[inputArr[i][2]]['title'] = inputArr[i][0];
+//           resultObj[inputArr[i][2]]['author'] = inputArr[i][1];
+//           resultObj[inputArr[i][2]]['isbn10'] = inputArr[i][2];
+//       }
+//       return resultObj
 //   }
-// }
 
-//var books = [
-// ["Learn to Program", "Chris Pine", 1934356360],
-//  ["Eloquent Javascript", "Marijn Haverbeke", 1593275846]
-]
-
-var bookData = function(inputArr){
-    var resultObj = {};
-    var subObj = {};
-    for (var i = 0; i < inputArr.length; i++){
-        resultObj[inputArr[i][2]] = {};
-        resultObj[inputArr[i][2]]['title'] = inputArr[i][0];
-        resultObj[inputArr[i][2]]['author'] = inputArr[i][1];
-        resultObj[inputArr[i][2]]['isbn10'] = inputArr[i][2];
-    }
-    return resultObj
-}
-
-//bookData(books)
-
-}
--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,7 +1,20 @@
 var myCode = require("../app");
 
-describe('intersection', function() {
-  it('returns an array of the common elements of two input arrays', function() {
-    expect(intersection([1,2,3,4,5],[3,4,7,8,9])).toEqual([3,4]);
+describe('bookData', function() {
+  it('takes nested arrays and turns it into nested objects', function() {
+    expect(myCode.bookData([ ["Learn to Program", "Chris Pine", 1934356360],["Eloquent Javascript", "Marijn Haverbeke", 1593275846]
+      ])).toEqual({"1934356360": {"title": "Learn to Program","author": "Chris Pine","isbn10": 1934356360},"1593275846": {"title": "Eloquent Javascript", "author": "Marijn Haverbeke","isbn10": 1593275846}});
   });
 });
+
+// OR
+
+// var bookData = require("../app");
+
+// describe('bookData', function() {
+//   it('takes nested arrays and turns it into nested objects', function() {
+//     expect(bookData([ ["Learn to Program", "Chris Pine", 1934356360],["Eloquent Javascript", "Marijn Haverbeke", 1593275846]
+//       ])).toEqual({"1934356360": {"title": "Learn to Program","author": "Chris Pine","isbn10": 1934356360},"1593275846": {"title": "Eloquent Javascript", "author": "Marijn Haverbeke","isbn10": 1593275846}});
+//   });
+// });
+
